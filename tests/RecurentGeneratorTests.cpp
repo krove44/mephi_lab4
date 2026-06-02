4016f18b06b6d8b2bfe5f9c5d2d92eae6aaabc76
krove44 <krove8@gmail.com>
2026-06-02 10:16:59 +0300

    more tests for recgen

--- tests/RecurentGeneratorTests.cpp
+++ tests/RecurentGeneratorTests.cpp
@@ -67,3 +67,69 @@ TEST(RecurentGeneratorArray, GetByIndexNonSequential) {
     EXPECT_EQ(gen.Get(Cardinal(0u)), 0);
     EXPECT_EQ(gen.Get(Cardinal(9u)), 34);
 }
+
+
+TEST(RecurentGeneratorArray, CurrentAfterGetNext) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed);
+    gen.GetNext();
+    gen.GetNext();
+    EXPECT_EQ(gen.Current(), 1);
+}
+
+TEST(RecurentGeneratorArray, CurrentBeforeGetNextThrows) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed);
+    EXPECT_THROW(gen.Current(), RecurentGeneratorException);
+}
+
+TEST(RecurentGeneratorArray, GetWithInfiniteIndexThrows) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed);
+    EXPECT_THROW(gen.Get(Cardinal::Omega()), RecurentGeneratorException);
+}
+
+TEST(RecurentGeneratorArray, FiniteGetNextBeyondSizeThrows) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed, 2);
+    gen.GetNext();
+    gen.GetNext();
+    EXPECT_THROW(gen.GetNext(), RecurentGeneratorException);
+}
+
+TEST(RecurentGeneratorArray, SlicePreservesValues) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed);
+    auto sliced = gen.Slice(Cardinal(2));
+    EXPECT_EQ(sliced->Get(Cardinal(0u)), 1);
+    EXPECT_EQ(sliced->Get(Cardinal(1u)), 2);
+}
+
+TEST(RecurentGeneratorArray, SliceWithInfiniteOffsetThrows) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed);
+    EXPECT_THROW(gen.Slice(Cardinal::Omega()), RecurentGeneratorException);
+}
+
+TEST(RecurentGeneratorArray, ArithmeticProgressionGetNext) {
+    ArraySequence<int> seed = {0};
+    RecurentGenerator<ArraySequence, int> gen(ArithmeticRule<ArraySequence>(3), seed);
+    EXPECT_EQ(gen.GetNext(), 0);
+    EXPECT_EQ(gen.GetNext(), 3);
+    EXPECT_EQ(gen.GetNext(), 6);
+    EXPECT_EQ(gen.GetNext(), 9);
+}
+
+TEST(RecurentGeneratorArray, ArithmeticProgressionGetByIndex) {
+    ArraySequence<int> seed = {0};
+    RecurentGenerator<ArraySequence, int> gen(ArithmeticRule<ArraySequence>(5), seed);
+    EXPECT_EQ(gen.Get(Cardinal(4u)), 20);
+}
+
+TEST(RecurentGeneratorArray, FiniteSliceHasCorrectSize) {
+    ArraySequence<int> seed = {0, 1};
+    RecurentGenerator<ArraySequence, int> gen(FibRule<ArraySequence>(), seed, 8);
+    auto sliced = gen.Slice(Cardinal(3u));
+    EXPECT_EQ(sliced->Size().Value(), size_t(5));
+}
+
